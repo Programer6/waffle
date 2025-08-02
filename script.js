78ab63cb496b96784ed9e2b00fa4a88a3b05aa64
Programer6 <delroyprithvi@gmail.com>
2025-08-02 20:39:37 +0200

    been cooking

---
+++ script.js
@@ -0,0 +1,23 @@
+const moods = {
+    happy: {
+        image: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXVmbmtvY3J5cGhwZmU1bHV3amU3cDQ5M3ZoN2tieWJ2OGpheTJ4aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11sBLVxNs7v6WA/giphy.gif',
+        quote: "For every minute you are angry you lose sixty seconds of happiness. - Ralph Waldo Emerson"
+    },
+    sad: {
+        image: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnA1ZG9pc2RjYzlhM3NhdDF5ZHp5bHBmbzlraDJ6ajZ4emFwc3VvMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9Y5BbDSkSTiY8/giphy.gif',
+        quote: "Tears come from the heart and not from the brain. - Leonardo da Vinci"
+    },
+    angry: {
+        image: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnRwM2ZleGx5Mjk1MmZ0ZGJtcnpsZzI5cDJvajV6MmxjaHk0bnhmZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT5LMVpIkWQasCA8eY/giphy.gif',
+        quote: "Speak when you are angry and you will make the best speech you will ever regret. - Ambrose Bierce"
+    },
+    motivated: {
+        image: 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHBzejNpcmVxa2FvcW55aGdpZndwYWhpZjZhZ2QweHhueGh0eGwwZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Qk2mdRUP9qJYrUS0KI/giphy.gif',
+        quote: "The only way to do great work is to love what you do. - Steve Jobs"
+    },
+    relaxed: {
+        image: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDl0bGJiZ2JweHp6bzd3YjhqcDBrc3gwbXF1ZjRod3FoZWRuaHlqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/J16yxZ0TUpSGqGTt3I/giphy.gif',
+        quote: "Tension is who you think you should be. Relaxation is who you are. - Chinese Proverb"
+    }
+};
+
